bd210eaf6efe2098df1934de5d592f6de097de6b
Dilya <dildoranasriddinzoda07@gmail.com>
2021-08-30 07:30:05 +0300

    first commit

---
+++ 111/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _111
+{
+    class Program
+    {
+         class Rectangle
+        {
+            public double side1, side2;
+
+            public Rectangle(double side1, double side2)
+            {
+                this.side1 = side1;
+                this.side2 = side2;
+            }
+
+            public double AreaCalculator()
+            {
+                return side1 * side2;
+            }
+            public double PerimeterCalculator()
+            {
+                return 2 * (side1 + side2);
+            }
+            public double Area
+            {
+                get { return AreaCalculator(); }
+            }
+
+            public double Perimeter
+            {
+                get { return PerimeterCalculator(); }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var p1 = Convert.ToDouble(Console.ReadLine());
+            var p2 = Convert.ToDouble(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(p1, p2);
+            Console.WriteLine($"Ploshad: {rectangle.Area},Parametr: {rectangle.Perimeter}");
+        }
+    }
+}
---
+++ 222/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace _222
+{
+ class Book
+    {
+        public Title title { get; set; }
+        public Author author { get; set; }
+        public Content content { get; set; }
+
+        public Book(string titleId, string authorId, string contentId)
+        {
+            title = new Title();
+            author = new Author();
+            content = new Content();
+            title.title = titleId;
+            author.author = authorId;
+            content.content = contentId;
+
+        }
+        public void Show()
+        {
+            title.Show();
+            author.Show();
+            content.Show();
+            
+        }
+    }
+    class Title
+    {
+        public string title = "";
+        public void Show()
+        {
+            Console.Write("Nazvanie: ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(title);
+            Console.ResetColor();
+        }
+    }
+    class Author
+    {
+        public string author = "";
+        public void Show()
+        {
+            Console.Write("Avtor: ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(author);
+            Console.ResetColor();
+        }
+    }
+    class Content
+    {
+        public string content = "";
+        public void Show()
+        {
+            Console.Write("content: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(content);
+            Console.ResetColor();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Вдали от безумной толпы", "Томас Харди", "Великая и вечная драма в отношений мужчины и женщины\n");
+            book.Show();
+
+            Book book1 = new Book("Психология лжи. Обмани меня если сможешь", "Пол Экман", "Научитесь замечать то, чего не видят другие.\n");
+            book1.Show();
+
+            Book book2 = new Book("Волшебная на всю голову", "Карина Микиртумова", "Если в жизни наступила чёрная полоса - нарисуй радугу и верь в себя.\n");
+            book2.Show();
+
+        }
+    }
+}
